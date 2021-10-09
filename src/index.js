f2ffd97132c248f4faf9e88ad24c1e3157c33860
Kazuomi Enami <kazuomienami@kazuomis-mbp.lan>
2021-10-09 15:41:01 -0400

    added thunk

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/stylesheet.css'
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './redux/reducer';
 import App from './components/app';
+import Thunk from 'redux-thunk'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
