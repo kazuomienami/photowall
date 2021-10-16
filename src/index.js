043d22805a9e78bc87225c7e582ee7a4f2a3e509
Kazuomi Enami <kazuomienami@Kazuomis-MBP.hsd1.mi.comcast.net>
2021-10-16 13:35:41 -0400

    added compose for redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/stylesheet.css'
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './redux/reducer';
 import App from './components/app';
-import Thunk from 'redux-thunk';
-import {database} from './database/config';
+import Thunk from 'redux-thunk'
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(Thunk))
+const enhancers = [applyMiddleware(Thunk)]
 
+const store = createStore(rootReducer, composeWithDevTools(...enhancers))
 ReactDOM.render(<Provider store = {store}><BrowserRouter><App/></BrowserRouter></Provider>, document.getElementById('root'));
